76c45b0fd9c875b8fb2898923f029cf34eb7d8ec
maclocalnetwork-cloud <macl.social.network@gmail.com>
2026-05-09 12:39:54 +0000

    fix: remplacement favicon par favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,8 +20,8 @@ export default function RootLayout({
   return (
     <html lang="fr" className={`${inter.variable} h-full antialiased`}>
       <head>
-        <link rel="icon" type="image/jpeg" href="/logo-ata.jpg?v=10" />
-        <link rel="shortcut icon" href="/logo-ata.jpg?v=10" />
+        <link rel="icon" type="image/x-icon" href="/favicon.ico?v=999" />
+        <link rel="shortcut icon" href="/favicon.ico?v=999" />
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
